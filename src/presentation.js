5e9e95e91504ad06829d5e02d8e67f4b9ce090d2
Will Klein <will.klein@gmail.com>
2018-06-19 02:15:58 -0600

    Draft slides

--- src/presentation.js
+++ src/presentation.js
@@ -41,59 +41,11 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
+        transition={['zoom', 'fade']}
+        transitionDuration={800}
         theme={theme}
       >
         {MarkdownSlides(slides)}
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
       </Deck>
     );
   }
--- src/slides.md
+++ src/slides.md
@@ -1,3 +1,214 @@
-# hi
+
+#### How Compilers, Linters, and Other Cool Things Work
+
 ---
-## hello
+
+#### Required: Node/NPM
+
+---
+
+node -v  
+v10.4.1
+
+npm -v  
+6.1.0
+
+---
+
+# The Project
+
+---
+
+git clone git@github.com:willklein/how-things-work.git
+
+---
+
+> npm install
+
+---
+
+# Install ESLint
+
+---
+
+### Exercise
+
+Install ESLint
+> npm install eslint@next --save-dev
+
+---
+
+# Configure ESLint
+
+---
+
+### Exercise
+
+Setup ESLint
+> npx eslint --init
+
+---
+
+### eslint:recommended
+
+Checked rules enabled:
+https://eslint.org/docs/rules/
+
+---
+
+# Add a Shared Config
+
+---
+
+### Exercise
+
+Search NPM for a config and set one up
+https://www.npmjs.com/search?q=eslint-config&ranking=popularity
+
+---
+
+# Add an ESLint Plugin
+
+---
+
+### Exercise
+
+Search NPM for a plugin and set one up
+https://www.npmjs.com/search?q=eslint-plugin
+
+---
+
+# Compilers
+
+---
+
+### How does this work?
+`
+// Life, Universe, and Everything
+var answer = 6 * 7;
+`  
+=>  
+`
+100011101010111010100011
+`
+
+---
+
+### Tokenize
+`
+[var] [answer] [=] [6] [*] [7][;]
+`
+
+---
+
+### Parse
+`
+[var] [answer] [=] [6] [*] [7][;]
+`  
+...  
+?
+
+---
+
+# Magical Tree
+
+---
+
+## Abstract...
+
+---
+
+## Syntax...
+
+---
+
+## Tree!
+
+---
+
+# Abstract Syntax Tree
+
+---
+
+http://astexplorer.net/
+
+---
+
+# The AST
+# is the Key
+
+---
+
+# Linters
+
+---
+
+# Write an ESLint Rule
+
+---
+
+# Write a Test Case
+
+---
+
+# Write a Rule from Scratch
+
+---
+
+# Compilers
+
+---
+
+# Babel
+
+---
+
+# Examine a Babel Plugin
+
+---
+
+# Setup a Custom Babel Plugin
+
+---
+
+# Write Our Own Babel Plugin
+
+---
+
+# Codemods
+
+---
+
+https://github.com/facebook/jscodeshift
+
+---
+
+# Cool Stuff
+
+---
+
+# Practical
+
+---
+
+# Scaling Tribal Knowledge
+
+---
+
+# Automate Conventions
+
+---
+
+# Challenge
+
+---
+
+Pick one:
+- write your OWN linting rule
+- implement a linting rule for xyz
+- write your OWN Babel plugin
+- implement a Babel plugin for xyz
+
+---
+
+# Go Forth & 10x
